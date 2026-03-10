6d354df07c1e1a8c1eda725fab3353b13ad6f0f7
Emilio Bodet <millobodet@MacBook-Air-de-Emilio.local>
2026-03-10 14:15:37 -0600

    10/03/2026

---
+++ json/json.js
@@ -0,0 +1,10 @@
+// BUSCAR LA RESPUESTA
+
+fetch('json.json')
+.then(Response=> Response.json())
+.then(json=>{
+    document.getElementById('adn').innerHTML=json.frase1;
+    document.getElementById('id').innerHTML=json.frase2;
+    document.getElementById('numero').innerHTML=json.cantidad1;
+          console.log(json)
+})
